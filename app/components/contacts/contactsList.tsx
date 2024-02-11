0fad7f9ccef48b7906fc2aee5f9dfe0156ef4f1a
b2 <brendan@mobilewebsmart.com>
2024-02-11 15:05:08 -0700

    contact main page

---
+++ app/components/contacts/contactsList.tsx
@@ -0,0 +1,10 @@
+export default function ContactsList() {
+	return (
+		<div>
+			<ul>
+				<li>John Doe</li>
+				<li>Jane Doe</li>
+			</ul>
+		</div>
+	);
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,18 @@
 import type { MetaFunction } from "@remix-run/node";
+import ContactsList from "~/components/contacts/contactsList";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Contacts" },
+    { name: "description", content: "Manage your list of personal contacts" },
   ];
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Contacts</h1>
+			<ContactsList />
     </div>
   );
 }
